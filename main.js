c1bdec7488da013e0dec1e48101109bf108de53e
Tim Jungmann <mail@timjungmann.com>
2022-10-06 21:01:14 +0200

    some changes

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-const el1 = document.getElementById('nl1');
-const el2 = document.getElementById('nl2');
-const el3 = document.getElementById('nl3');
-const el4 = document.getElementById('nl4');
-const el5 = document.getElementById('nl5');
-const el6 = document.getElementById('nl6');
-const el7 = document.getElementById('nl7');
-const el8 = document.getElementById('nl8');
-const el9 = document.getElementById('nl9');
-const el10 = document.getElementById('nl10');
+const hereElements = document.getElementsByClassName("js-here-elem");
 
-let elements = [el1, el2, el3, el4, el5, el6, el7, el8, el9, el10]
-
-for(let element of elements){
-  element.onclick = function(){
-    element.classList.add('underline');
-  }
-  element.onauxclick = function(){
-    element.classList.add('underline');
-  }
-}
+Array.from(hereElements).forEach((el) => {
+    el.onclick = function () {
+        el.classList.add("underline");
+    };
+    el.onauxclick = function () {
+        el.classList.add("underline");
+    };
+});
